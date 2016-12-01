5d6c5ccaa3fda38d1da3a32934f5d4d486411c72
sc2830704 <sc2830704@gmail.com>
2016-12-01 23:28:17 +0800

    index.html + prepare.php

--- views/prepare.php
+++ views/prepare.php
@@ -7,16 +7,28 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="/content/stylesheets/global.css" />
-    <link rel="stylesheet" href="/content/stylesheets/prepare.css" />
+    <link rel="stylesheet" href="../content/stylesheets/global.css" />
+    <link rel="stylesheet" href="../content/stylesheets/prepare.css" />
 </head>
 <body>
     <div class="header-container">
         <div class="header-title">
-            寒分大富翁
+            微。跡<img class="logo" src="../taiwan.png" hspace="30">
         </div>
-    </div><div class="main-container">
-        <h1>Prepare</h1>
+    </div>
+	<div class="main-container" >
+		<div class=".guide">
+			<h1>
+			<div>挑戰賽說明</div>
+			<div>
+				<p>1.答對題目可前進一格 </p>
+				<p>2.答錯或跳過題目會倒退一格</p>
+				<p>3.題目為四選一的選擇題 </p>
+				<p>4.最先抵達終點的隊伍獲勝 </p>
+				<p>5.遊戲時間為15分鐘</p>
+			</h1>
+			<button type="submit" value="" onclick="">準備好了!</button>
+		</div>
     </div>
     <footer></footer>
 </body>
