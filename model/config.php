74d9ccd757f76b4bdfd0dea220a43f7049cfc9ac
WangChen Wu <ra960618@gmail.com>
2016-12-08 18:58:15 +0800

    DB config test

--- model/config.php
+++ model/config.php
@@ -1,43 +1,11 @@
 <?php
-    
-    $connstr = getenv("MS_TableConnectionString");
-    function connStrToArray($conn_str){
-        echo $conn_str;
-        // Initialize array.
-        $conn_array = array();
- 
-        // Split conn string on semicolons. Results in array of "parts".
-        $parts = explode(";", $conn_str);
- 
- 
-        // Loop through array of parts. (Each part is a string.)
-        foreach($parts as $part){
- 
-            // Separate each string on equals sign. Results in array of 2 items.
-            $temp = explode("=", $part);
- 
-            // Make items key=>value pairs in returned array.
-            $conn_array[$temp[0]] = $temp[1];
-        }
-    
-        return $conn_array;
-    }
-    function OpenConnection()  
-    {
-        $connArr = connStrToArray(connstr);
-        try  
-        {  
-            $serverName = connArr[Data Source];  
-            $connectionOptions = array("Database"=>connArr[Initial Catalog],  
-                "Uid"=>connArr[User ID], "PWD"=>connArr[Password]);  
-            $conn = sqlsrv_connect($serverName, $connectionOptions);  
-            if($conn == false)  
-                die(FormatErrors(sqlsrv_errors()));
-            
-        }
-        catch(Exception $e)
-        {  
-            echo("Error!");
-        }  
-    }
+    try {
+    $conn = new PDO("sqlsrv:server = tcp:msseed13winter.database.windows.net,1433; Database = msseed13winter", "msseed13", "Ms131313");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "success!"
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 ?>
