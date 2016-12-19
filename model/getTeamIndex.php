b171f1dd48d47fa95f9ef9d81bd021fe5fc83e25
sam961124 <sam961124@gmail.com>
2016-12-20 02:28:16 +0800

    clean trash

--- model/getTeamIndex.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once "connectDB.php";
-    $postdata = file_get_contents("php://input");
-    $request = json_decode($postdata);
-    $_db = new DB();
-    echo $_db->getTeamIndex($request->name);
-?>
