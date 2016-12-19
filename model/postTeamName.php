7a8ac59add94ccc8edcc12c08a73eee7d05de4b3
sam961124 <sam961124@gmail.com>
2016-12-19 23:36:43 +0800

    post team name

--- model/postTeamName.php
+++ model/postTeamName.php
@@ -3,6 +3,6 @@
     $postdata = file_get_contents("php://input");
     $request = json_decode($postdata);
     $_db = new DB();
-    $_db->postTeamName($request->name);
-    echo "Post Success";
+    $result = $_db->postTeamName($request->name);
+    echo $result;
 ?>
