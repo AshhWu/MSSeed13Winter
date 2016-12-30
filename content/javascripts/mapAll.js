a134c1d8405a7554d5ffcb11caf916fa463a4a78
love6214261 <chou610252@gmail.com>
2016-12-30 17:26:23 +0800

    addAllCar

--- content/javascripts/mapAll.js
+++ content/javascripts/mapAll.js
@@ -1,8 +1,9 @@
 
 var c,cxt;
 var currentImgMainX,currentImgMainY;
-var img,imgMain,imgMain2,imgMain3,imgMain4;
-var count1,count2,count3,count4;
+var img,imgMain,imgMain2,imgMain3,imgMain4,imgMain5;
+var count1,count2,count3,count4,count5;
+var team1,team2,team3,team4,team5;
 
 window.onload=function()
 {
@@ -17,20 +18,54 @@ window.onload=function()
     imgMain3.src="../images/car3.png";
     imgMain4=new Image();
     imgMain4.src="../images/car4.png";
+    imgMain5=new Image();
+    imgMain5.src="../images/car5.png";
 
     currentImgMainX=461;
     currentImgMainY=30;
     count1=0;
     count2=0;
     count3=0;
-    count4=0; 
+    count4=0;
+    count5=0;
     imgMain.onload=function()
     {
         cxt.drawImage(imgMain,0,0,256,256,370,40,50,50);
+        getTeamAndScore();
     };
-    document.getElementById("talkBox").innerHTML="第一組:"+count1+"<br>"+"第二組:"+count2+"<br>"+"第三組:"+count3+"<br>"+"第四組:"+count4+"<br>";
+    document.getElementById("talkBox").innerHTML=team1+":"+count1+"<br>"+team2+":"+count2+"<br>"+team3+":"+count3+"<br>"+team4+":"+count4+"<br>"+team5+":"+count5;
 
 };
+
+
+function getTeamAndScore() {
+    var URLs = "../model/getAllCarAndScore.php";//this one
+    return $.ajax({
+        url: URLs,
+        data: null,
+        type: "post",
+        dataType: "json",//回傳資料用json檔
+        success: function (data) {
+
+            team1= data[0][1];
+            count1=data[0][2];
+            team2= data[1][1];
+            count2=data[1][2];
+            team3= data[2][1];
+            count3=data[2][2];
+            /*
+            team4= data[3][1];
+            count4=data[3][2];
+            team5= data[4][1];
+            count5=data[4][2];
+            */
+
+        },
+        error: function (err) {
+            alert(err.responseText);
+        }
+    });
+}
 
 window.onkeydown=move;
 
@@ -723,5 +758,171 @@ function countNum()
                
                 break;
         }
-    document.getElementById("talkBox").innerHTML="第一組:"+count1+"<br>"+"第二組:"+count2+"<br>"+"第三組:"+count3+"<br>"+"第四組:"+count4+"<br>";
+    switch (count5)
+    {
+        case undefined:
+            cxt.drawImage(imgMain5,0,0,256,256,370,40,50,50);
+            break;
+        case 0:
+
+            cxt.drawImage(imgMain5,0,0,256,256,370,40,50,50);
+
+            break;
+        case 1:
+
+            cxt.drawImage(imgMain5,0,0,256,256,380,70,50,50);
+
+            break;
+        case 2:
+
+            cxt.drawImage(imgMain5,0,0,256,256,320,50,50,50);
+
+            break;
+        case 3:
+
+            cxt.drawImage(imgMain5,0,0,256,256,320,120,50,50);
+
+            break;
+        case 4:
+
+            cxt.drawImage(imgMain5,0,0,256,256,280,90,50,50);
+
+            break;
+        case 5:
+
+            cxt.drawImage(imgMain5,0,0,256,256,260,140,50,50);
+
+            break;
+        case 6:
+
+            cxt.drawImage(imgMain5,0,0,256,256,300,190,50,50);
+
+            break;
+        case 7:
+
+            cxt.drawImage(imgMain5,0,0,256,256,220,200,50,50);
+
+            break;
+        case 8:
+
+            cxt.drawImage(imgMain5,0,0,256,256,300,230,50,50);
+
+            break;
+        case 9:
+
+            cxt.drawImage(imgMain5,0,0,256,256,270,270,50,50);
+
+            break;
+        case 10:
+
+            cxt.drawImage(imgMain5,0,0,256,256,190,255,50,50);
+
+            break;
+        case 11:
+
+            cxt.drawImage(imgMain5,0,0,256,256,170,300,50,50);
+
+            break;
+        case 12:
+
+            cxt.drawImage(imgMain5,0,0,256,256,220,350,50,50);
+
+            break;
+        case 13:
+
+            cxt.drawImage(imgMain5,0,0,256,256,180,345,50,50);
+
+            break;
+        case 14:
+
+            cxt.drawImage(imgMain5,0,0,256,256,160,400,50,50);
+
+            break;
+        case 15:
+
+            cxt.drawImage(imgMain5,0,0,256,256,230,400,50,50);
+
+            break;
+        case 16:
+
+            cxt.drawImage(imgMain5,0,0,256,256,150,470,50,50);
+
+            break;
+        case 17:
+
+            cxt.drawImage(imgMain5,0,0,256,256,200,500,50,50);
+
+            break;
+        case 18:
+
+            cxt.drawImage(imgMain5,0,0,256,256,230,550,50,50);
+
+            break;
+        case 19:
+
+            cxt.drawImage(imgMain5,0,0,256,256,245,600,50,50);
+
+            break;
+        case 20:
+
+            cxt.drawImage(imgMain5,0,0,256,256,250,530,50,50);
+
+            break;
+        case 21:
+
+            cxt.drawImage(imgMain5,0,0,256,256,270,490,50,50);
+
+            break;
+        case 22:
+
+            cxt.drawImage(imgMain5,0,0,256,256,290,420,50,50);
+
+            break;
+        case 23:
+
+            cxt.drawImage(imgMain5,0,0,256,256,320,370,50,50);
+
+            break;
+        case 24:
+
+            cxt.drawImage(imgMain5,0,0,256,256,350,280,50,50);
+
+            break;
+        case 25:
+
+            cxt.drawImage(imgMain5,0,0,256,256,360,220,50,50);
+
+            break;
+        case 26:
+
+            cxt.drawImage(imgMain5,0,0,256,256,400,130,50,50);
+
+            break;
+        case 27:
+
+            cxt.drawImage(imgMain5,0,0,256,256,400,60,50,50);
+
+            break;
+        case 28:
+
+            cxt.drawImage(imgMain5,0,0,256,256,400,30,50,50);
+
+            break;
+        case 29:
+
+            cxt.drawImage(imgMain5,0,0,256,256,380,0,50,50);
+
+            break;
+        case 30:
+
+            cxt.drawImage(imgMain5,0,0,256,256,370,40,50,50);
+
+            break;
+        case 31:
+            count1=30;
+
+            break;
+    }
+
+    document.getElementById("talkBox").innerHTML=team1+":"+count1+"<br>"+team2+":"+count2+"<br>"+team3+":"+count3+"<br>"+team4+":"+count4+"<br>"+team5+":"+count5;
 }
---
+++ model/getAllCarAndScore.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AllenHsu
+ * Date: 2016/12/30
+ * Time: 下午 03:57
+ */
+
+include_once "connectDB.php";
+
+$_db = new DB();
+
+$result = $_db->getAllScores();
+
+# JSON-encode the response
+echo $json_response = json_encode($result);
