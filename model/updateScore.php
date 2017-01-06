e79f8781267a13c659da3272660fb5a3a66d38b7
sam961124 <sam961124@gmail.com>
2017-01-06 11:40:59 +0800

    update score debug

--- model/updateScore.php
+++ model/updateScore.php
@@ -3,5 +3,7 @@
     $postdata = file_get_contents("php://input");
     $request = json_decode($postdata);
     $_db = new DB();
+    echo $request->team;
+    echo $request->score;
     $_db->updateScore($request->team, $request->score);
 ?>
