96506e41ca7fd7d2533821f9e6d63a664ca338f4
sam961124 <sam961124@gmail.com>
2017-01-13 15:17:03 +0800

    encode

--- model/postTeamName.php
+++ model/postTeamName.php
@@ -3,6 +3,7 @@
     $postdata = file_get_contents("php://input");
     $request = json_decode($postdata);
     $_db = new DB();
-    $result = $_db->postTeamName($request->name);
+    $name = utf8_encode($request->name);
+    $result = $_db->postTeamName($name);
     echo $json_response = json_encode($result, JSON_UNESCAPED_UNICODE);
 ?>
