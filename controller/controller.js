3cbdb976e8eddefa556d14ba43ab0890d7dfa58f
sam961124 <sam961124@gmail.com>
2017-01-17 23:37:34 +0800

    rank debug

--- controller/controller.js
+++ controller/controller.js
@@ -51,7 +51,7 @@ app.controller('rankController', ['$scope', '$http', '$window', function($scope,
     $scope.ftime = [];
     $(function(){
         $http.get('../model/getRank.php')
-            .then(function(response){
+            .then(function(data){
                 console.log("Get Rank Success!");
                 for(var i=0;i<5;i++)
                 {
