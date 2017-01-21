7ea62ef5d842a60bf2832db274cb2cc3e56151fb
sam961124 <sam961124@gmail.com>
2017-01-21 18:32:36 +0800

    fix bug

--- controller/controller.js
+++ controller/controller.js
@@ -1,7 +1,7 @@
 var app = angular.module('app', []);
 var timer;
 var gameClock;
-var gameFinished = false; //set this to true let game.html locate to gameover page
+
 app.controller('indexController', ['$scope', '$http', '$window', function($scope, $http, $window){
     $scope.teamName = "";
     $scope.postTeamName = function(){
@@ -139,10 +139,10 @@ app.controller('gameController', ['$scope', '$http', '$window', function($scope,
     function gameTimer(){
 		startTime = new Date();
 		var clock=0;
-		setInterval(function() {
+		$scope.gameFinishCheck = setInterval(function() {
 			var clock = (new Date()-startTime)/1000;
 			var gameClock = getTime(clock.toFixed(0));
-            if (gameFinished) {
+            if ($scope.score >= 30) {
 				$window.sessionStorage.time = gameClock;
                 var request = $http({
                     method: "post",
@@ -154,10 +154,11 @@ app.controller('gameController', ['$scope', '$http', '$window', function($scope,
                     headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
                 });
                 request.success(function () {
+                    clearInterval($scope.gameFinishCheck);
                     console.log("Time Updated!")
+                    $window.sessionStorage.score = $scope.score;
+    				window.location.href="arrive.html";
                 });
-				$window.sessionStorage.score = $scope.score;
-				window.location.href="gameover.html";
 			}else if(clock.toFixed(0) >= 1200){
 				$window.sessionStorage.time = gameClock;
                 var request = $http({
@@ -169,8 +170,11 @@ app.controller('gameController', ['$scope', '$http', '$window', function($scope,
                     },
                     headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
                 });
-				$window.sessionStorage.score = $scope.score;
-				window.location.href="gameover.html";
+                request.success(function () {
+                    console.log("Time Updated!")
+                    $window.sessionStorage.score = $scope.score;
+    				window.location.href="gameover.html";
+                });
 			}else{
 				$('#clock').html(gameClock);
 			}
@@ -312,7 +316,7 @@ var c;
 var currentImgMainX,currentImgMainY;
 var img,imgMain;
 
-window.onload=function()
+/*window.onload=function()
 {
     c=document.getElementById("myCanvas");
     $scope.cxt=c.getContext("2d");
@@ -325,8 +329,19 @@ window.onload=function()
     {
         $scope.cxt.drawImage(imgMain,0,0,256,256,461,40,50,50);
     };
-};
+};*/
 $(function(){
+    c=document.getElementById("myCanvas");
+    $scope.cxt=c.getContext("2d");
+    //主角
+    imgMain=new Image();
+    imgMain.src="../images/car"+ $window.sessionStorage.tNum + ".png";
+    currentImgMainX=461;
+    currentImgMainY=30;
+    imgMain.onload=function()
+    {
+        $scope.cxt.drawImage(imgMain,0,0,256,256,461,40,50,50);
+    };
     $('.loading-title').addClass('animated slideInLeft');
     $('.loading-title').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
         $(this).removeClass('animated');
@@ -485,7 +500,7 @@ function move(count)
                 break;
             case 30:
                 $scope.cxt.drawImage(imgMain,0,0,256,256,461,40,50,50);
-                gameFinished = true;
+
                 break;
             case 31:
                 count=30;
